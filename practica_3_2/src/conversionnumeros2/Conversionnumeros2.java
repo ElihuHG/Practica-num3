15147cb8f7468e194b6b859fdba7da846cba175a
joepo <joepo@DESKTOP-KFUGBBM>
2020-04-14 21:48:48 -0500

    practica_3

---
+++ practica_3_2/src/conversionnumeros2/Conversionnumeros2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversionnumeros2;
+
+/**
+ *
+ * @author joepo
+ */
+public class Conversionnumeros2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+
+       Frmprincipal frmPrincipal= new Frmprincipal();
+       frmPrincipal.setVisible(true);
+       
+    }
+    }
+    
+
